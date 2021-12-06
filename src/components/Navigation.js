b2c80f9531208b8b6f6c3688b23bb1a4a05d4cad
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2021-12-06 06:46:27 +0100

    nav menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,47 @@
 import React from "react";
-import {Link} from "react-router-dom"
-import Button from "../fragments/Button"
-import "../styles/NavList.css"
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Button from "../fragments/Button";
+import "../styles/NavList.css";
 
 const Navigation = () => {
   return (
-  <>
+    <>
+      <FaBars className="nav-menu" />
       <ul className="nav-list">
-          <li className="nav-item"><Link className="nav-link" to="">fees</Link> </li>
-          <li className="nav-item"><Link className="nav-link" to="">learn</Link> </li>
-          <li className="nav-item"><Link className="nav-link" to="">blog</Link> </li>
-          <li className="nav-item"><Link className="nav-link" to="">for business</Link> </li>
-          <li className="nav-item"><Link className="nav-link" to=""><Button text="log in" smallBorder={true} /></Link> </li>
-          <li className="nav-item"><Link className="nav-link" to=""><Button text="sign up" small={true} /></Link> </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="">
+            fees
+          </Link>{" "}
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="">
+            learn
+          </Link>{" "}
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="">
+            blog
+          </Link>{" "}
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="">
+            for business
+          </Link>{" "}
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="">
+            <Button text="log in" smallBorder={true} />
+          </Link>{" "}
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="">
+            <Button text="sign up" small={true} />
+          </Link>{" "}
+        </li>
       </ul>
-  </>);
+    </>
+  );
 };
 
 export default Navigation;
