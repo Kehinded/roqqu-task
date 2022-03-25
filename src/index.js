3eee93672e08b0d2a177a48b206c8a9bdd18d5da
Kehinded <ayenikehinded@gmail.com>
2022-03-25 09:25:11 +0100

    react snap

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+
+import { hydrate, render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import {SidebarContextProvider} from "./store/SidebarContext"
-import {HelmetProvider } from 'react-helmet-async';
+import { SidebarContextProvider } from "./store/SidebarContext";
+import { HelmetProvider } from "react-helmet-async";
 
-ReactDOM.render(
+const APP = (
   <BrowserRouter>
     <SidebarContextProvider>
       <React.StrictMode>
         <HelmetProvider>
-           <App />
+          <App />
         </HelmetProvider>
       </React.StrictMode>
     </SidebarContextProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
+
+// ReactDOM.render(
+// ,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
